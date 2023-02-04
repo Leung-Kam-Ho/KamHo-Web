ab2b706fce6539c095bb4b4b3c08459c525c413a
Leung-Kam-Ho <20051119d@connect.polyu.hk>
2023-02-04 16:05:16 +0800

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'firebase_options.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Firebase App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final databaseReference = FirebaseDatabase.instance.ref("Testing");
+
+  String _message = '';
+  String _displayMessage = '';
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    databaseReference.onValue.listen((event) {
+      setState(() {
+        count++;
+        _displayMessage = count.toString();
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Firebase App'),
+      ),
+      body: Column(
+        children: [
+          TextField(
+            onChanged: (value) {
+              setState(() {
+                _message = value;
+              });
+            },
+          ),
+          MaterialButton(
+            onPressed: () async {
+              await databaseReference.set({
+                'message': _message,
+              });
+              
+            },
+            child: const Text('Send'),
+          ),
+          Text(_displayMessage)
+        ],
+      ),
+    );
+  }
+}

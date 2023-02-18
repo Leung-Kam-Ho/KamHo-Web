8baded5fbbc7405b93b914b1c5f8ad8049635f0d
Leung-Kam-Ho <20051119d@connect.polyu.hk>
2023-02-18 17:34:18 +0800

    using google material symbol

--- lib/sidePanel.dart
+++ lib/sidePanel.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SidePanel extends StatefulWidget {
   const SidePanel({Key? key}) : super(key: key);
@@ -10,11 +11,18 @@ class SidePanel extends StatefulWidget {
 class _SidePanelState extends State<SidePanel> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-        width: 100,
-        alignment: Alignment.topCenter,
-        color: Colors.transparent,
-        child:  Text("=",style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30))
-      );
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+          width: 100,
+          alignment: Alignment.topCenter,
+          color: Colors.transparent,
+          child: IconButton(
+        // Use the MdiIcons class for the IconData
+        icon: SvgPicture.asset("MaterialDesign/menu_FILL0_wght400_GRAD200_opsz48.svg"), 
+        onPressed: () { 
+          print('Using the sword'); }
+       )),
+    );
   }
 }
